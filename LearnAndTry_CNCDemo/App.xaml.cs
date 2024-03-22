6537be004e9583a3eee3174ace38688945f7ec3f
YoudiasGray <youdiasGray@outlook.com>
2024-03-22 18:07:12 +0800

    update

--- LearnAndTry_CNCDemo/App.xaml.cs
+++ LearnAndTry_CNCDemo/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace LearnAndTry_CNCDema
+namespace LearnAndTry_CNCDemo
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- LearnAndTry_CNCDemo/MainWindow.xaml.cs
+++ LearnAndTry_CNCDemo/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LearnAndTry_CNCDema
+namespace LearnAndTry_CNCDemo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
