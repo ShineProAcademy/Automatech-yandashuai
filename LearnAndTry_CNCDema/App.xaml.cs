24f4606f0c1ef2698822577a324b1b9f6990a736
YoudiasGray <youdiasGray@outlook.com>
2024-03-22 18:04:38 +0800

    新增项目

---
+++ LearnAndTry_CNCDema/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LearnAndTry_CNCDema
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
